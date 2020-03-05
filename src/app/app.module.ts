7ce17aefcd9ba99895a309a52d64a61e6fe33bc2
shaux <54446329+Shaurya-Sarma@users.noreply.github.com>
2020-03-04 22:22:23 -0500

    Added Login

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+
+import { MatIconModule } from "@angular/material/icon";
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
