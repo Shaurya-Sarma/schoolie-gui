8876d1cc3e21973e7e8ad9ce12455b084643d9ec
shaux <54446329+Shaurya-Sarma@users.noreply.github.com>
2020-04-15 16:06:46 -0400

    Updated Calendar UI

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -8,31 +8,6 @@ import { Component, OnInit } from "@angular/core";
 export class CalendarComponent implements OnInit {
   curDate: Date = new Date();
 
-  // firstDay: number = new Date(
-  //   this.curDate.getFullYear(),
-  //   this.curDate.getMonth()
-  // ).getDay();
-
-  daysOfWeek: Array<string> = [
-    "Sun",
-    "Mon",
-    "Tues",
-    "Wed",
-    "Thurs",
-    "Fri",
-    "Sat",
-  ];
-
-  endDate: number = new Date(
-    this.curDate.getFullYear(),
-    this.curDate.getMonth() + 1,
-    0
-  ).getDate();
-
-  dates: Array<number> = [...Array(this.endDate).keys()].map(
-    (number) => number + 1
-  );
-
   months: Array<string> = [
     "January",
     "February",
@@ -48,7 +23,53 @@ export class CalendarComponent implements OnInit {
     "December",
   ];
 
+  daysOfWeek: Array<string> = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat",
+  ];
+
+  renderCalendar() {}
+  firstDay: number = new Date(
+    this.curDate.getFullYear(),
+    this.curDate.getMonth()
+  ).getDay();
+
+  endDate: number = new Date(
+    this.curDate.getFullYear(),
+    this.curDate.getMonth() + 1,
+    0
+  ).getDate();
+
+  dates: Array<any> = [...Array(this.endDate).keys()].map(
+    (number) => number + 1
+  );
+
+  addOuterDates(arr: Array<any>) {
+    //* Adding Dates For Prev Month
+    for (let i = this.firstDay; i > 0; i--) {
+      arr.unshift(" ");
+    }
+    //* Adding Dates For Next Month
+    for (let j = 35 - arr.length; j > 0; j--) {
+      arr.push(" ");
+    }
+  }
+
+  changeDate(direction: string) {
+    if (direction === "prev") {
+      this.curDate.setMonth(this.curDate.getMonth() - 1);
+    } else if (direction === "after") {
+    }
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.addOuterDates(this.dates);
+  }
 }
