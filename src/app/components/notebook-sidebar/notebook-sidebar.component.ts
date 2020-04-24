1845ccc8cbc6bd256bfba3509666fc90eb7f4d0a
shaux <54446329+Shaurya-Sarma@users.noreply.github.com>
2020-04-24 18:19:06 -0400

    Added Search Bar Field

--- src/app/components/notebook-sidebar/notebook-sidebar.component.ts
+++ src/app/components/notebook-sidebar/notebook-sidebar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-notebook-sidebar',
-  templateUrl: './notebook-sidebar.component.html',
-  styleUrls: ['./notebook-sidebar.component.scss']
+  selector: "app-notebook-sidebar",
+  templateUrl: "./notebook-sidebar.component.html",
+  styleUrls: ["./notebook-sidebar.component.scss"],
 })
 export class NotebookSidebarComponent implements OnInit {
+  searchForm: FormGroup;
+  value: string;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.searchForm = this.fb.group({});
   }
-
 }
